2bbccb3e26fcc6c2d53ac542efd842e38fa496f1
LaercioLima <laerciotot10@gmail.com>
2022-12-25 20:46:14 -0300

    insert colors and text styles

---
+++ lib/app/core/utils/style/colors/general_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kColorBackground = Color(0xFFFFFFFF);
+const Color kColorPrimary = Colors.black;
+const Color kColorBackgroundUser = Colors.lightBlue;
+const Color kColorPrimaryAdmin = Colors.white;
+const Color kColorBackgroundUserAdmin = Colors.grey;
