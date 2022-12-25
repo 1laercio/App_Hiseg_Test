dba7adcbcccbfa550347f3c924e9de9f1e1adac7
LaercioLima <laerciotot10@gmail.com>
2022-12-25 20:02:28 -0300

    insert strings constants

---
+++ lib/app/core/utils/constants/app_strings.dart
@@ -0,0 +1,22 @@
+///CORE
+const kAppName = 'Hiseg';
+
+///LOGIN
+const kLogin = "Login";
+const kUserCPF = "Informe seu CPF";
+const kPassword = "Informe sua senha";
+
+///LOCALIZATION
+const kPermission = "Conceder autorização de acesso á localização ?";
+const kYes = "Sim";
+const kNot = "Não";
+const kAlert = "Autorização necessária para uso da aplicação";
+
+///HOME
+const kUserName = "Nome:";
+const kLocalizaton = "Localização:";
+const kAge = "Idade:";
+const kUserType = "Tipo de Usuário:";
+const kSave = "Salvar";
+const kCancel = "Cancelar";
+const kSaveAlert = "Deseja realmente salvar as alterações no Usuário?";
